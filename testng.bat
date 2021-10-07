d286047eb4d3ddf3a6412fb5fd0458df58c52035
Sateendra <92027295+Sateendra@users.noreply.github.com>
2021-10-07 10:21:06 +0530

    Delete testng.bat

--- testng.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\satee\eclipse-workspace\DemoProject
-java -cp C:\Users\satee\eclipse-workspace\DemoProject\lib\*;C:\Users\satee\eclipse-workspace\DemoProject\bin org.testng.TestNG C:\Users\satee\eclipse-workspace\DemoProject\testng.xml
